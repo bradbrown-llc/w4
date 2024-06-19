25a201a2e71e5fc1141638d39e51a3359fd699ca
Brad Brown <bradbrown@bradbrown.llc>
2024-06-19 08:28:55 +0000

    remove unused

--- lib/ejra/methods/traceTx.ts
+++ lib/ejra/methods/traceTx.ts
@@ -1,12 +1,4 @@
 import * as jra from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/jra@0.1.1/mod.ts'
-import * as schemas from "../schemas/mod.ts";
-
-type TraceConfig = {
-    enableMemory:boolean
-    disableStack:boolean
-    disableStorage:boolean
-    enableReturnData:boolean
-}
 
 export async function traceTx(rpc: string, hash:string) {
   const method = "debug_traceTransaction";
